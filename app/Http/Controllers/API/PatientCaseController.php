bf113650babd23f867bf943bc0b029515f3db386
faheem-at-747754686602 <muhammad.faheem@ozoneddigital.com>
2024-06-04 23:52:57 +0500

    sdfsdf

--- app/Http/Controllers/API/PatientCaseController.php
+++ app/Http/Controllers/API/PatientCaseController.php
@@ -357,7 +357,7 @@ class PatientCaseController extends Controller
             $patient_cases->planner_id = $user->id;
         }
         if(!empty(auth()->user()->roles) && !empty(auth()->user()->roles) && !empty(auth()->user()->roles[0]) && auth()->user()->roles[0]['name'] && auth()->user()->roles[0]['name'] == 'treatment_planner'){
-            $patient_cases->planner_id = auth()->user()->id;
+            $patient_cases->qa_id = auth()->user()->id;
         }
         $patient_cases->save();
 
@@ -446,7 +446,7 @@ class PatientCaseController extends Controller
             $patient_cases->planner_id = $user->id;
         }
         if(!empty(auth()->user()->roles) && !empty(auth()->user()->roles) && !empty(auth()->user()->roles[0]) && auth()->user()->roles[0]['name'] && auth()->user()->roles[0]['name'] == 'quality_check'){
-            $patient_cases->planner_id = auth()->user()->id;
+            $patient_cases->qa_id = auth()->user()->id;
         }
         $patient_cases->save();
 
