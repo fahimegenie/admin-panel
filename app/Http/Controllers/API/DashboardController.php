c8de01e5cc3d6ce20a0fb40dbc4408a8af42eef7
fahim-w <fahim.egenie@gmail.com>
2024-06-29 22:57:17 -0400

    upda

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -26,32 +26,10 @@ class DashboardController extends Controller
 
     public function adminDashboard(){
 
-        // $startWeek = Carbon::now()->subWeek()->startOfWeek(); // 30 May 2024
-        // $endWeek   = Carbon::now()->subWeek()->endOfWeek();  // 5 June 2024
-
-        // $orders    = Order::query()->whereBetween('created_at',[ $startWeek,$endWeek ])->get();
-
-
         $patient_cases = [];
-        $patient_cases = User::withCount(['created_user', 'users', 'planner', 'qa', 'post_processing'])->where('id', auth()->user()->id)->first();
-        
-
-        $patient_cases['cases_count'] = PatientCase::count();
-        $patient_cases['cases_0_count'] = PatientCase::where('status', 0)->count();
-        $patient_cases['cases_1_count'] = PatientCase::where('status', 1)->count();
-        $patient_cases['cases_2_count'] = PatientCase::where('status', 2)->count();
-        $patient_cases['cases_3_count'] = PatientCase::where('status', 3)->count();
-        $patient_cases['cases_4_count'] = PatientCase::where('status', 4)->count();
-        $patient_cases['cases_5_count'] = PatientCase::where('status', 5)->count();
-        $patient_cases['cases_6_count'] = PatientCase::where('status', 6)->count();
-        $patient_cases['cases_7_count'] = PatientCase::where('status', 7)->count();
-        $patient_cases['cases_8_count'] = PatientCase::where('status', 8)->count();
-        $patient_cases['cases_9_count'] = PatientCase::where('status', 9)->count();
-        $patient_cases['cases_10_count'] = PatientCase::where('status', 10)->count();
-        $patient_cases['cases_11_count'] = PatientCase::where('status', 11)->count();
-        $patient_cases['cases_12_count'] = PatientCase::where('status', 12)->count();
-        
-
+            $patient_cases = User::withCount(['created_user', 'users', 'planner', 'qa', 'post_processing'])->where('id', auth()->user()->id)->first();
+            $cases_count = PatientCase::count();
+        $patient_cases['cases_count'] = $cases_count;
         if(empty($patient_cases)){
             $this->status = 400;
             $this->response['status'] = $this->status;
